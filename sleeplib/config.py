ac3541ba30a80a696a8d83e1f0d5db2347a20b7b
Richard Cui <Cui.Jie@mayo.edu>
2025-07-31 16:27:01 -0500

    Fix formatting and update comments in config.py for clarity

--- sleeplib/config.py
+++ sleeplib/config.py
@@ -2,29 +2,29 @@
 
 from dataclasses import dataclass
 
+
 @dataclass
 class Config:
 
     # Data params
-    PATH_FILES_BONOBO: str = 'your_path' #'
-    PATH_LUT_BONOBO: str = 'your_path' #'lut_labelled_20230628.csv'
-    PATH_CONTINOUS_EEG: str = 'your_path/continuousEEG'
+    PATH_FILES_BONOBO: str = "your_path"  #'
+    PATH_LUT_BONOBO: str = "lut_labelled_20230628.csv"  # data table
+    PATH_CONTINOUS_EEG: str = "your_path/continuousEEG"
 
-    FQ: int = 128 # Hz
-    
-    
-    # Preprocessing 
-    MONTAGE: str = 'combine'
-    WINDOWSIZE: float = 1 # 2 seconds (cut length of EEG signals)
-    
-	# Model parameters
-    N_CHANNELS: int = 37 #19+18
+    FQ: int = 128  # Hz
+
+    # Preprocessing
+    MONTAGE: str = "combine"
+    WINDOWSIZE: float = 1  # 2 seconds (cut length of EEG signals)
+
+    # Model parameters
+    N_CHANNELS: int = 37  # 19+18
 
     # training parameters
-    BATCH_SIZE: int = 256 #test 128
-    LR: float = 1e-4 #test 1e-4
+    BATCH_SIZE: int = 256  # test 128
+    LR: float = 1e-4  # test 1e-4
 
     def print_config(self):
-        print('THIS CONFIG FILE CONTAINS THE FOLLOWING PARAMETERS :\n')
+        print("THIS CONFIG FILE CONTAINS THE FOLLOWING PARAMETERS :\n")
         for key, value in self.__dict__.items():
             print(key, value)
