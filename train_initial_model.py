6c9005af1be1872286f3877973b30632cfde6048
Richard Cui <Cui.Jie@mayo.edu>
2025-09-12 13:41:09 -0500

    Update model checkpoint filename in train_initial_model.py for clarity

--- train_initial_model.py
+++ train_initial_model.py
@@ -105,7 +105,7 @@ wandb_logger = WandbLogger(project="spikenet2_project", name="spikenet2_run")
 # create callbacks with early stopping and model checkpoint (saves the best model)
 callbacks = [
     EarlyStopping(monitor="val_loss", patience=5),
-    ModelCheckpoint(dirpath=model_path, filename="weights", monitor="val_loss"),
+    ModelCheckpoint(dirpath=model_path, filename="heardmine", monitor="val_loss"),
 ]
 # create trainer, use fast dev run to test the code
 trainer = pl.Trainer(
