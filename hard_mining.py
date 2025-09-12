7c95881e9e7c077d873a4248e658ac894aa5253b
Richard Cui <Cui.Jie@mayo.edu>
2025-09-12 17:32:34 -0500

    Add initial implementation of hard-negative sample extraction

---
+++ hard_mining.py
@@ -0,0 +1,16 @@
+# get hard-negative samples
+
+# 2025 Richard J. Cui. Modified: Fri 09/12/2025 04:16:14.055411 PM
+# $Revision: 0.1 $  $Date: Fri 09/12/2025 04:16:14.055411 PM $
+#
+# Mayo Clinic Foundation
+# Rochester, MN 55901, USA
+#
+# Email: Cui.Jie@mayo.edu
+
+# imports
+import numpy as np
+import pandas as pd
+import mat73
+from tqdm import tqdm
+import os
