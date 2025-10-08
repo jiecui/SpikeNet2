0aef7216bf981d636b95fa6039aa72b511fcc2a8
Richard Cui <Cui.Jie@mayo.edu>
2025-10-08 14:16:42 -0500

    refactor: update cluster length parameter for hard mining process

--- hard_mining_parallel.py
+++ hard_mining_parallel.py
@@ -36,7 +36,7 @@ time_step = 8
 window_length = 3  # in seconds
 num_points = sampling_rate * window_length
 threshold = 0.5  # .8, 0.75, 0.5, 0.4; 1st round set at .8
-cluster_len = 10  # 10 8 5 3
+cluster_len = 3  # 10 8 5 3
 
 
 def hard_mine(df):
